ba58c93c3b2e0ccf3b571b68fd1ad55c7690d505
GuiomArt7 <ada_hack243@hackademy.mx>
2024-03-01 19:47:52 -0600

    feat: añadir un Seeder

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categorias')->insert([
+            'nombre' => 'Desayunos',
+            'icono' => 'desayuno',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Vegetarianos',
+            'icono' => 'ensalada',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Chilaquiles',
+            'icono' => 'chilaquiles',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Postres',
+            'icono' => 'postre',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Bebidas',
+            'icono' => 'cafe',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
