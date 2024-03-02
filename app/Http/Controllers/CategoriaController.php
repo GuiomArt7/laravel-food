99eb792bea0597eb4ca34a6200ee03af8479ee51
GuiomArt7 <ada_hack243@hackademy.mx>
2024-03-01 20:29:09 -0600

    feat: crear un API Resource para Categorías

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Categoria;
+use App\Http\Resources\CategoriaCollection;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
     public function index() {
-        return response()->json(['categorias' => Categoria::all()]);
+        //return response()->json(['categorias' => Categoria::all()]);
+
+        return new CategoriaCollection(Categoria::all());
     }
 }
