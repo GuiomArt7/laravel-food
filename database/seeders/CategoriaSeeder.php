c106f78db9afbe00e2854a52ba436aa46b1f45ee
GuiomArt7 <ada_hack243@hackademy.mx>
2024-03-04 16:36:51 -0600

    feat: consumir las categorías en react

--- database/seeders/CategoriaSeeder.php
+++ database/seeders/CategoriaSeeder.php
@@ -3,9 +3,10 @@
 namespace Database\Seeders;
 
 use Carbon\Carbon;
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+
 
 class CategoriaSeeder extends Seeder
 {
@@ -49,4 +50,4 @@ class CategoriaSeeder extends Seeder
             'updated_at' => Carbon::now(),
         ]);
     }
-}
+} 
