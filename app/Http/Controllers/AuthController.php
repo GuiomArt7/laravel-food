a1872232bf553afcfaed903420ce788c80992768
GuiomArt7 <ada_hack243@hackademy.mx>
2024-03-04 22:43:07 -0600

    feat: Crear un Request de Autenticación

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegistroRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     
-    public function register(Request $request) {
+    public function register(RegistroRequest $request) {
+        //Validar el Registro
+        $data = $request->validated();
         
     }
 
