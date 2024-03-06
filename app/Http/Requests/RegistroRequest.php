1762a8e2c565ca00e29a360cbcfaf76fe5465f68
GuiomArt7 <ada_hack243@hackademy.mx>
2024-03-05 18:03:41 -0600

    fix: cierre de corchete

--- app/Http/Requests/RegistroRequest.php
+++ app/Http/Requests/RegistroRequest.php
@@ -27,8 +27,20 @@ class RegistroRequest extends FormRequest
             'email' => ['required', 'email', 'unique:users,email'],
             'password' => [
                 'required',
-                'confirmed'],
+                'confirmed',
                 PasswordRules::min(8)->letters()->symbols()->numbers()
+            ]
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name' => 'El Nombre es obligatorio',
+            'email.required' => 'El Email es obligatorio',
+            'email.email' => 'El email no es válido',
+            'email.unique' => 'El usuario ya está registrado',
+            'password' => 'El password debe contener al menos 8 caracteres, un símbolo y un número'
         ];
     }
 }
