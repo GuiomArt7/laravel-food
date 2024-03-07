5aebaacb17920cb44042d4a105ed1cb2936ac955
GuiomArt7 <ada_hack243@hackademy.mx>
2024-03-06 18:58:01 -0600

    feat: CORS y crear el usuario

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegistroRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -11,6 +12,19 @@ class AuthController extends Controller
     public function register(RegistroRequest $request) {
         //Validar el Registro
         $data = $request->validated();
+
+        //crear el usuario
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        //Retornar una respuesta
+        return [
+            'token' => $user->crateToken('token')->plainTextToken,
+            'user' => $user
+        ];
         
     }
 
