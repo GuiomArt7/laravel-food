00288490d2f0dd5793c623da31bd10cdfa1749cb
GuiomArt7 <ada_hack243@hackademy.mx>
2024-05-24 14:39:41 -0600

    add: mensaje de confirmación de contraseña

--- app/Http/Requests/RegistroRequest.php
+++ app/Http/Requests/RegistroRequest.php
@@ -40,7 +40,8 @@ class RegistroRequest extends FormRequest
             'email.required' => 'El Email es obligatorio',
             'email.email' => 'El email no es válido',
             'email.unique' => 'El usuario ya está registrado',
-            'password' => 'La contraseña debe contener al menos 8 caracteres, un símbolo y un número'
+            'password' => 'La contraseña debe contener al menos 8 caracteres, un símbolo y un número',
+            'password.confirmed' => 'Escriba de nuevo su contraseña'
         ];
     }
 }
